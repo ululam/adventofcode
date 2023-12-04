13fce5a95c4da36186ed8b39200241c08bbf1d82
Alex <oleksii.zakharchenko@bitso.com>
2023-12-03 17:15:57 -0800

    Part 2 ready

--- 2023/3/task3.py
+++ 2023/3/task3.py
@@ -1,42 +1,66 @@
-# Read line by triples
+# (1) Read line by triples
 # Walk current line charwise. If faced a digit, make number: read until next char is not digit
 # Check the number is adj: if any of its chars is adj: left, right, bottom, up, lup, rup, ldown, rdown
 # For that, keep number -> list of it's char positions.
 # If any of nubmer's char adj -> add number to res
 # Continue with char next to the number's last char.
 # Walk until the EoL
+# (2) Essentially similar approach. but check numbers above, below and around
+#
+# Note: There are negative numbers. But all 5 of them are not "parts", so I removed logic dealing with negatives
+
+def collect_line_numbers(line: str) -> list((int, int, int)):
+    res, poses, digits = [], [], []
+    for i in range(len(line)):
+        if line[i].isdigit():
+            digits.append(line[i])
+            poses.append(i)
+        elif digits:
+            n = int(''.join(digits))
+            res.append((n, poses[0], poses[-1]))
+            digits, poses = [], []
+    # Don't forget last number in line
+    if digits:
+        n = int(''.join(digits))
+        res.append((n, poses[0], poses[-1]))
+    return res
 
 def collect_adj_numbers(prev_line, current_line, next_line):
-    poses, digits = [], []
     summa = 0
-    for i in range(len(current_line)):
-        if current_line[i].isdigit():
-            # Check for negative number
-            # if not digits and i > 0 and current_line[i-1] == '-':
-            #     poses.append(i-1)
-            #     digits.append("-")
-            digits.append(current_line[i])
-            poses.append(i)
-        else:
-            summa += check_number(digits, poses, prev_line, current_line, next_line)
-            digits, poses = [], []
-    # Don't forget to check last number in the line
-    return summa + check_number(digits, poses, prev_line, current_line, next_line)
-
-def check_number(digits, poses, prev_line, current_line, next_line):
-    if digits:
-        if any([has_adj_chars(i, [prev_line, next_line]) for i in poses]) \
-                or is_char(poses[0] - 1, current_line) \
-                or is_char(poses[-1] + 1, current_line):
-            return int(''.join(digits))
-    return 0
+    for n, start, end in collect_line_numbers(current_line):
+        if any([has_adj_chars(i, [prev_line, next_line]) for i in range(start, end + 1)]) \
+                or is_char(start - 1, current_line) \
+                or is_char(end + 1, current_line):
+            summa += n
+    return summa
 
 def has_adj_chars(i: int, lines):
     return any([is_char(j, s) for j in [i - 1, i, i + 1] for s in lines])
 
 def is_char(i: int, s: str):
     return s is not None and 0 <= i < len(s) and s[i] != '.'
-        # and (not s[i] == "-" or (i >= len(s) or not s[i+1].isdigit()))
+
+# ------ 2 ------
+def find_gears(prev_line, current_line, next_line):
+    summa = 0
+    for i in range(len(current_line)):
+        if current_line[i] == '*':
+            numbers = get_adj_numbers_to_position(i, [prev_line, current_line, next_line])
+            if len(numbers) == 2:
+                summa += numbers[0] * numbers[1]
+    return summa
+
+def get_adj_numbers_to_position(i: int, lines):
+    if not lines[0] or not lines[1]:
+        return []
+    res = []
+    # # It's not the time for binary search, ok
+    for line in lines:
+        for n, start, end in collect_line_numbers(line):
+            if i in range(start - 1, end + 2):
+                res.append(n)
+    return res
+
 
 def solution(f, fun):
     summa, current_line, next_line = 0, None, None
@@ -45,10 +69,10 @@ def solution(f, fun):
         if current_line:
             summa += fun(prev_line, current_line, next_line)
     # Process last line
-    summa += collect_adj_numbers(current_line, next_line, None)
+    summa += fun(current_line, next_line, None)
     print(summa)
 
 
-# with open("test3.txt") as f:
 with open("3.input") as f:
-    solution(f, collect_adj_numbers)
+    # solution(f, collect_adj_numbers)
+    solution(f, find_gears)
