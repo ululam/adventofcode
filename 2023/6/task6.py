9f1392d6f717cd1e07cb718775bb5995bd99e980
Alex <oleksii.zakharchenko@bitso.com>
2023-12-05 22:31:59 -0800

    Enhanced version

--- 2023/6/task6.py
+++ 2023/6/task6.py
@@ -1,15 +1,14 @@
-from math import sqrt, floor, ceil
+from math import sqrt
 
 def find_best_times(T, dist):
-    t1 = (T - sqrt(T*T - 4*dist))/2
-    t2 = (T + sqrt(T*T - 4*dist))/2
-    t1, t2 = int(floor(t1)), int(ceil(t2))
-    t1 += 1 if not check(t1, T, dist) else 0
+    sqrt_d = sqrt(T * T - 4 * dist)
+    t1, t2 = round(T - sqrt_d) >> 1, round(T + sqrt_d) >> 1
+    t1 += 1 if not check(t1, T, dist) else 0  # We need to check real boundary, as we operate INTs, not real numbers
     t2 -= 1 if not check(t2, T, dist) else 0
     return t2 - t1 + 1
 
-def check(t, T, D):
-    return (T - t) * t > D
+def check(t, T, dist):
+    return (T - t) * t > dist
 
 def part1(f):
     times = [int(n) for n in to_numbers(next(f))]
